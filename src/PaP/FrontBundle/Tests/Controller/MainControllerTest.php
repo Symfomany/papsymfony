8ea7997ca7fe66ce063b0de689a84121831b0a1c
Your Name <you@example.com>
2015-12-04 10:23:39 +0100

    add changes

---
+++ src/PaP/FrontBundle/Tests/Controller/MainControllerTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace PaP\FrontBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class MainControllerTest extends WebTestCase
+{
+    public function testIndex()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/');
+
+        $this->assertTrue($crawler->filter('html:contains("We\'re Getting Ready To Launch")')->count() > 0);
+    }
+
+}
